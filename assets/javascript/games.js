fa0beb5603230d2b8eba8a68da6cdf382c5c2242
Kenya <kenya55@msn.com>
2019-08-07 14:40:32 -0400

    added crystal images, updated index file with buttons, score text, and jame url

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,3 +1,4 @@
 var target_min = 19, target_max = 120, score_min = 1, score_max = 12;
 var crystal1_value = 0, crystal2_value = 0, crystal3_value = 0, crystal4_value = 0;
 var target_value = 0, score_value = 0, wins = 0, loses = 0;
+
